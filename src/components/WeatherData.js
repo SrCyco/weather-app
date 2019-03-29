d36c6d2f686e54bd2ce3357ac43fe88faf93f846
Sergio Nino <sergioanino@qdata.io>
2019-03-29 18:33:40 -0500

    propType pocation.propTypes = {

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature';
+import {
+    SUN,
+} from '../constants/weathers';
 
 const WheaterData = () => (
     <div>
-        <WeatherTemperature temperature={20} weatherState='algo'></WeatherTemperature>
+        <WeatherTemperature temperature={20} weatherState={SUN}></WeatherTemperature>
         <WeatherExtraInfo humidity={80} wind={'10 m/s'}></WeatherExtraInfo>
     </div>
 );
